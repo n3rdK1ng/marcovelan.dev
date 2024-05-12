b61444570596afea3c084e0fb2035c373f1351f4
Marco Velan <47577181+n3rdK1ng@users.noreply.github.com>
2024-05-13 01:34:46 +0200

    feat: add content (#2)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,13 @@ import type { Config } from 'tailwindcss'
 const config = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		container: {
+			center: true,
+			padding: '24px',
+			screens: {
+				lg: '1024px',
+			},
+		},
 		extend: {},
 	},
 	plugins: [],
