75eedb707fdd6ce9c5a46bf81782742e078f6d03
Marco <marco.velan10@gmail.com>
2024-05-13 18:08:39 +0200

    feat: integrate i18n w/ czech translations

---
+++ src/i18n/ui.ts
@@ -0,0 +1,88 @@
+export const languages = {
+	en: '🇺🇸',
+	cs: '🇨🇿',
+}
+
+export const defaultLang = 'en'
+
+export const ui = {
+	en: {
+		resume: 'Resume',
+		viewGithub: 'View source on GitHub',
+		hero1:
+			'I am a software developer with a passion for creating user-friendly interfaces.',
+		hero2:
+			'My current focus is on web and mobile front-end development, where I prefer to work with React and TypeScript.',
+		contactInfo: 'Contact Information',
+		openCalendar: 'Open Calendar',
+		experience: 'Experience',
+		czechRepublic: 'Czech Republic',
+		blazeExperience:
+			'At Blaze company I worked on various web/mobile projects for several clients. Such as XALARM, a new service with a mobile application for personal safety. Another notable project was Izzy, an application designed to simplify the process of scheduling and ordering home cleaning services.',
+		royalFoxExperience:
+			'Frontend engineer on the team responsible for creating the next revolution in the learning industry. ForClassmates is a platform that connects students and teachers in a new way.',
+		selfEmployed: 'Self-employed',
+		freelanceExperience:
+			'I started my journey in the world of software development by creating and maintaining websites for local businesses. I am always choosing the cutting edge technologies to make sure my clients are getting the best possible product.',
+		languages: 'Languages',
+		czech: 'Czech',
+		native: 'Native speaker',
+		english: 'English',
+		comfort: 'Comfortable spoken and written',
+		croatian: 'Croatian',
+		commuticable: 'Able to comunicate',
+		spanish: 'Spanish',
+		basic: 'Bit more than the basics',
+		hobbies: 'Hobbies',
+		hobby1: 'Discovering the nature',
+		hobby2: 'Automating the world around me',
+		hobby3: 'Tech Discovery and Mastery',
+		hobby4: 'Working on my healthy mindset',
+		achievemets: 'Achievements',
+		achievement1: 'Went on the same voyage as my namesake.',
+		achievement2:
+			'Allowed by the Czech court to do adult stuff before the age of 18.',
+		achievement3: 'I have lifted 300kg several times on my own.',
+		present: 'present',
+	},
+	cs: {
+		resume: 'Životopis',
+		viewGithub: 'Zobrazit zdrojový kód na GitHubu',
+		hero1:
+			'Jsem softwarový vývojář s nadšením pro vytváření uživatelsky přívětivých prostředí.',
+		hero2:
+			'V současné době se zaměřuji na front-end vývoj webových a mobilních aplikací, kde nejraději využívám React a TypeScript.',
+		contactInfo: 'Kontaktní informace',
+		openCalendar: 'Otevřít kalendář',
+		experience: 'Zkušenosti',
+		czechRepublic: 'Česká republika',
+		blazeExperience:
+			'V Blaze company jsem pracoval na různých webových/mobilních projektech pro řadu klientů. Například XALARM, nová služba s mobilní aplikací pro osobní bezpečí. Dalším důležitým projektem byla aplikace Izzy, která má usnadnit proces plánování a objednávání úklidových služeb pro domácnosti.',
+		royalFoxExperience:
+			'Frontend engineer v týmu, který připravuje revoluci ve vzdělávacím průmyslu. ForClassmates je platforma, s níž se novým přístupem propojují studenti a učitelé.',
+		selfEmployed: 'Živnostník',
+		freelanceExperience:
+			'Svou profesionální kariéru ve světě vývoje softwaru jsem začal tvořením a údržbou webových stránek pro místní podniky. Vždy si vybírám nejmodernější technologie, abych se ujistil, že mí zákazníci dostanou ten nejlepší možný výsledek.',
+		languages: 'Jazyky',
+		czech: 'Čeština',
+		native: 'Rodilý mluvčí',
+		english: 'Angličtina',
+		comfort: 'Pohodlný mluvený a psaný projev',
+		croatian: 'Chorvatština',
+		commuticable: 'Schopen komunikovat',
+		spanish: 'Španělština',
+		basic: 'Trochu víc než jen základy',
+		hobbies: 'Koníčky',
+		hobby1: 'Objevování přírody',
+		hobby2: 'Automatizace světa kolem mě',
+		hobby3: 'Objevování a ovládání technologií',
+		hobby4: 'Práce na mém zdravém myšlení',
+		achievemets: 'Úspěchy',
+		achievement1: 'Vydal jsem se na stejnou cestu jako můj jmenovec.',
+		achievement2: 'Byl jsem českým soudem emancipován před dosažením 18 let.',
+		achievement3: 'Opakovaně jsem vlastní silou zvedl 300 kg.',
+		present: 'současnost',
+	},
+} as const
+
+export const showDefaultLang = false
